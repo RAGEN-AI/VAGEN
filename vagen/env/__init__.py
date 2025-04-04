12ab36e0509212aba332c91613456848b3486654
YaningDylan <20082585d@gmail.com>
2025-04-04 21:16:22 +0000

    merge env

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -1,4 +1,5 @@
 from vagen.env.register import REGISTERED_ENVS, register
 
 from vagen.env.sokoban.env import SokobanInterface
+from vagen.env.svg.env import SVGInterface
 from vagen.env.eb_alfred.env import ALFREDInterface
